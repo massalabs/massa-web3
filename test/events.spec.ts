9e40e953af23f2e42fd7c6784ab58fb6eda101ac
EvgeniPirianov <evgeni.pirianov@level01.io>
2022-05-15 02:11:19 +0300

    refactored examples

---
+++ test/events.spec.ts
@@ -0,0 +1,16 @@
+import "mocha";
+import { assert } from "chai";
+import { Test } from "mocha";
+
+describe("Test Suite - web3 sdk", () => {
+	describe("Test case - Massa Events (Filtering & Polling)", () => {
+
+		it("Serialize & Deserialize.", () => {
+			assert(true);
+		});
+
+		it("Send a basic payload.", async () => {
+			assert(true);
+		});
+	});
+});
