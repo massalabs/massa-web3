9511eb25d35f9e416ddaa06ab3d4845eac311569
EvgeniPirianov <evgeni.pirianov@level01.io>
2022-08-25 16:46:47 +0300

    updated description for is_final field

--- src/interfaces/IEventFilter.ts
+++ src/interfaces/IEventFilter.ts
@@ -6,6 +6,6 @@ export interface IEventFilter {
 	emitter_address: null | string; // Address
 	original_caller_address: null | string; // Address
 	original_operation_id: null | string; // operation id
-	/// is_final value (true=resolves to only candidate events, false => resolves to only final events, null => resolves to final and candidate events)
+	/// is_final value (true=resolves to only final events, false => resolves to only candidate events, null => resolves to final and candidate events)
 	is_final: boolean | null;
 }
