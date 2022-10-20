e767269dcc9f5e7ff82ddd20c38cde33b32d3e08
EvgeniPirianov <evgeni.pirianov@level01.io>
2022-10-20 18:10:47 +0300

    extended tests

--- examples/smartContracts/index.ts
+++ examples/smartContracts/index.ts
@@ -1,6 +1,9 @@
 import { IAccount } from "../../src/interfaces/IAccount";
 import { IContractData } from "../../src/interfaces/IContractData";
+import { IEventFilter } from "../../src/interfaces/IEventFilter";
 import { ClientFactory, DefaultProviderUrls } from "../../src/web3/ClientFactory";
+import { IEvent } from "../../src/interfaces/IEvent";
+import { IReadData } from "../../src/interfaces/IReadData";
 import { WalletClient } from "../../src/web3/WalletClient";
 import { deploySmartContract } from "./deployer";
 
@@ -36,6 +39,41 @@ const DEPLOYER_SECRET_KEY = "S13iFJarF4v6CxYPeguUQHqkxDdGZgFhrsiEMznbnS3is9aXFps
             datastore: datastoreMap
         } as IContractData, web3Client, true, deployerAccount);
         spinner.succeed(`Deployed Smart Contract ${chalk.green("successfully")} with opId ${deploymentOperationId}`);
+
+        // poll smart contract events for the opId
+        spinner = ora(`Filtering for sc events....`).start();
+        const eventsFilter = {
+            start: null,
+            end: null,
+            original_caller_address: null,
+            original_operation_id: deploymentOperationId,
+            emitter_address: null,
+        } as IEventFilter;
+
+        const events: Array<IEvent> = await web3Client.smartContracts().getFilteredScOutputEvents(eventsFilter);
+        spinner.succeed(`Sc events received: ${chalk.yellow(JSON.stringify(events, null, 4))}`);
+
+        // find an event that contains the emitted sc address
+        spinner = ora(`Extracting deployed sc address from events....`).start();
+        const addressEvent: IEvent|undefined = events.find(event => event.data.includes("Address:"));
+        if (!addressEvent) {
+            throw new Error("No events were emitted from contract containing a message `Address:...`. Please make sure to include such a message in order to fetch the sc address");
+        }
+        const scAddress: string = addressEvent.data.split(":")[1];
+        spinner.succeed(`Smart Contract Address: ${chalk.yellow(scAddress)}`);
+
+        // finally get some read state
+        spinner = ora(`Reading a smart contract state...`).start();
+        const readTxId = await web3Client.smartContracts().readSmartContract({
+            fee: 0,
+            maxGas: 200000,
+            simulatedGasPrice: 0,
+            targetAddress: scAddress,
+            targetFunction: "balanceOf",
+            parameter: deployerAccount.address,
+        } as IReadData);
+        const readScOperationId = readTxId[0];
+        spinner.succeed(`Called read contract with operation ID ${chalk.yellow(JSON.stringify(readScOperationId, null, 4))}`);
     } catch (ex) {
         const msg = chalk.red(`Error = ${ex}`);
         if (spinner) spinner.fail(msg);
