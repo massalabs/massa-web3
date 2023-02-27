78424a1ce5bb5d82aead88c29234877beb1d4ed5
EvgeniPirianov <evgeni.pirianov@level01.io>
2023-02-27 11:08:55 +0200

    added new massa conversions

---
+++ src/web3/MassaAmount.ts
@@ -0,0 +1,65 @@
+import BigNumber from 'bignumber.js';
+
+export enum MASSA_UNIT {
+  MASSA,
+  MILLI_MASSA, // 10-3
+  MICRO_MASSA, // 10-6
+  NANO_MASSA, // 10-9
+}
+
+// smallest massa unit
+const MASSA_SCALING_FACTOR = 9;
+
+export class MassaAmount {
+  private value: BigNumber;
+  private unit: MASSA_UNIT;
+
+  constructor(value: BigNumber | number | string, unit: MASSA_UNIT) {
+    // truncate value to smallest massa unit
+    this.value = new BigNumber(new BigNumber(value).toFixed(MASSA_SCALING_FACTOR));
+    this.unit = unit;
+  }
+
+  public getValue(): BigNumber {
+    return this.value;
+  }
+
+  public getUnit(): MASSA_UNIT {
+    return this.unit;
+  }
+
+  public toRolls(): MassaAmount {
+    const valueInMassa = this.toUnit(MASSA_UNIT.MASSA);
+    return new MassaAmount(valueInMassa.value.toFixed(0), MASSA_UNIT.MASSA);
+  }
+
+  public toGas(): MassaAmount {
+    return new MassaAmount(this.value.toFixed(0), this.unit);
+  }
+
+  public toCoins(): MassaAmount {
+    return this.toUnit(MASSA_UNIT.NANO_MASSA);
+  }
+
+  public toString(): string {
+    return `${this.value.toFixed()} ${MASSA_UNIT[this.unit]}`;
+  }
+
+  public toUnit(targetUnit: MASSA_UNIT): MassaAmount {
+    if (this.unit === targetUnit) {
+      return this;
+    }
+
+    const diff = Math.abs(this.unit - targetUnit);
+
+    if (this.unit < targetUnit) {
+      // Scale up
+      const scale = new BigNumber(10).pow(diff * 3);
+      return new MassaAmount(this.value.multipliedBy(scale), targetUnit);
+    } else {
+      // Scale down
+      const scale = new BigNumber(10).pow(diff * 3);
+      return new MassaAmount(this.value.dividedBy(scale), targetUnit);
+    }
+  }
+}
---
+++ test/conversion.spec.ts
@@ -0,0 +1,81 @@
+import 'mocha';
+import { expect } from 'chai';
+import { MassaAmount, MASSA_UNIT } from '../src';
+
+describe('MassaAmount Class', () => {
+  it('classical unit test conversion cases', () => {
+    const amount = new MassaAmount(1, MASSA_UNIT.MASSA);
+    expect(amount.toString()).to.equal("1 MASSA");
+
+    const milliMassa = amount.toUnit(MASSA_UNIT.MILLI_MASSA);
+    expect(milliMassa.toString()).to.equal("1000 MILLI_MASSA");
+
+    const microMassa = amount.toUnit(MASSA_UNIT.MICRO_MASSA);
+    expect(microMassa.toString()).to.equal("1000000 MICRO_MASSA");
+
+    const nanoMassa = amount.toUnit(MASSA_UNIT.NANO_MASSA);
+    expect(nanoMassa.toString()).to.equal("1000000000 NANO_MASSA");
+  });
+
+  it('test conversion cases with lower than accepted decimal precision', () => {
+    // amount has precision < 9 dp.
+    const amount = new MassaAmount(1.523, MASSA_UNIT.MASSA);
+    expect(amount.toString()).to.equal("1.523 MASSA");
+
+    const milliMassa = amount.toUnit(MASSA_UNIT.MILLI_MASSA);
+    expect(milliMassa.toString()).to.equal("1523 MILLI_MASSA");
+
+    const microMassa = amount.toUnit(MASSA_UNIT.MICRO_MASSA);
+    expect(microMassa.toString()).to.equal("1523000 MICRO_MASSA");
+
+    const nanoMassa = amount.toUnit(MASSA_UNIT.NANO_MASSA);
+    expect(nanoMassa.toString()).to.equal("1523000000 NANO_MASSA");
+  });
+
+  it('test conversion cases with higher than accepted decimal precision', () => {
+    // amount has precision > 9 dp.
+    const amount = new MassaAmount(1.5238762359, MASSA_UNIT.MASSA);
+    expect(amount.toString()).to.equal("1.523876236 MASSA");
+
+    const milliMassa = amount.toUnit(MASSA_UNIT.MILLI_MASSA);
+    expect(milliMassa.toString()).to.equal("1523.876236 MILLI_MASSA");
+
+    const microMassa = amount.toUnit(MASSA_UNIT.MICRO_MASSA);
+    expect(microMassa.toString()).to.equal("1523876.236 MICRO_MASSA");
+
+    const nanoMassa = amount.toUnit(MASSA_UNIT.NANO_MASSA);
+    expect(nanoMassa.toString()).to.equal("1523876236 NANO_MASSA");
+  });
+
+  it('test conversion to rolls', () => {
+    const rolls1 = new MassaAmount(1.52, MASSA_UNIT.MASSA);
+    expect(rolls1.toRolls().toString()).to.equal("2 MASSA");
+
+    const rolls2 = new MassaAmount(1.1, MASSA_UNIT.MASSA);
+    expect(rolls2.toRolls().toString()).to.equal("1 MASSA");
+
+    const rolls3 = new MassaAmount(1.9999999, MASSA_UNIT.MASSA);
+    expect(rolls3.toRolls().toString()).to.equal("2 MASSA");
+
+    const rolls4 = new MassaAmount(0, MASSA_UNIT.MASSA);
+    expect(rolls4.toRolls().toString()).to.equal("0 MASSA");
+
+    const rolls5 = new MassaAmount(1523876236, MASSA_UNIT.NANO_MASSA);
+    expect(rolls5.toRolls().toString()).to.equal("2 MASSA");
+  });
+
+
+  it('test conversion to gas', () => {
+    const gas1 = new MassaAmount(1.52, MASSA_UNIT.MASSA);
+    expect(gas1.toGas().toString()).to.equal("2 MASSA");
+
+    const gas2 = new MassaAmount(2_500_000, MASSA_UNIT.MASSA);
+    expect(gas2.toGas().toString()).to.equal("2500000 MASSA");
+
+    const gas3 = new MassaAmount(2500000, MASSA_UNIT.NANO_MASSA);
+    expect(gas3.toGas().toString()).to.equal("2500000 NANO_MASSA");
+
+    const gas4 = new MassaAmount(2500000897.7547, MASSA_UNIT.NANO_MASSA);
+    expect(gas4.toGas().toString()).to.equal("2500000898 NANO_MASSA");
+  });
+});
