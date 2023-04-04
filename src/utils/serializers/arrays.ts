b49e096160e8e28101b8bd2ecac188121d5974af
EvgeniPirianov <evgeni.pirianov@level01.io>
2023-04-04 10:01:35 +0300

    Revert "added typed arrays draft"
    This reverts commit b1f0f52a42eed48e61ff0ccfd1d0011f95068a79.

--- src/utils/serializers/arrays.ts
+++ src/utils/serializers/arrays.ts
@@ -2,7 +2,6 @@ import {
   IDeserializedResult,
   ISerializable,
 } from '../../interfaces/ISerializable';
-import { TypedArrayUnit } from '../arguments';
 
 export function serializableObjectsArrayToBytes<T extends ISerializable<T>>(
   source: T[],
@@ -39,74 +38,4 @@ export function deserializeObj<T extends ISerializable<T>>(
 ): IDeserializedResult<T> {
   const deserialized = new Clazz().deserialize(data, offset);
   return deserialized;
-}
-
-/**
- * Converts a Uint8Array into an Array of deserialized type parameters T.
- *
- * @param source - the Uint8Array to convert
- * @param Clazz - the class constructor prototype T.prototype
- *
- * @return {T[]} an array of deserialized T's
- */
-export function bytesToSerializableObjectArray<T extends ISerializable<T>>(
-  source: Uint8Array,
-  Clazz: new () => T,
-): T[] {
-  const array: T[] = [];
-  let offset = 0;
-
-  while (offset < source.length) {
-    let deserializationResult = deserializeObj(source, offset, Clazz);
-    offset = deserializationResult.offset;
-    array.push(deserializationResult.instance);
-  }
-
-  return array;
-}
-
-// ===================================================
-
-/**
- * Convert an array of type parameter to StaticArray<u8>
- *
- * @remarks
- * This will perform a deep copy only for native types.
- * inspired by https://github.com/AssemblyScript/assemblyscript/blob/main/std/assembly/array.ts#L69-L81
- *
- * @see {@link Serializable}
- *
- * @param source - the array to convert
- */
-export function nativeTypeArrayToBytes<
-  T extends ArrayLike<TypedArrayUnit | Uint8Array | string | boolean>,
->(source: T[]): Uint8Array {
-  const elementSize = new DataView(new ArrayBuffer(1)); // .setUint8(0, 0).byteOffset; // size of each element in bytes
-  const sourceLength = source.length;
-
-  // ensures that the new array has the proper length.
-  let targetLength = sourceLength * 2; // elementSize;
-
-  // allocates a new Uint8Array in the memory
-  let target = new Uint8Array(targetLength);
-
-  for (let i = 0; i < sourceLength; i++) {
-    switch (typeof source[i]) {
-      case 'bigint':
-        break;
-      case 'boolean':
-        break;
-      case 'object':
-        if (source[i] === null) {
-          // nothing to copy
-        } else {
-          throw new Error(`Unsupported type ${typeof source[i]}`);
-        }
-        break;
-      default:
-        throw new Error(`Unsupported type ${typeof source[i]}`);
-    }
-  }
-
-  return target;
 }
