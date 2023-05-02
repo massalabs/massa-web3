9ca5ed6d8466ed84b0f7ea073c2d32bd939279b5
unknown <adriancik01@gmail.com>
2023-05-02 15:16:23 +0200

    added more detail

--- src/interfaces/IClientConfig.ts
+++ src/interfaces/IClientConfig.ts
@@ -5,6 +5,11 @@ import { IProvider } from './IProvider';
  * @remarks
  * This interface is used to configure the client with an array of providers,
  * retry strategy, period offset, and ping timeout settings.
+ *
+ * @see providers of type `Array<IProvider>` is an array of providers used for API requests.
+ * @see retryStrategyOn of type `boolean` (optional) is a flag to enable or disable the retry strategy.
+ * @see periodOffset of type `number | null` is the period offset in seconds for syncing with the blockchain.
+ * @see pingTimeoutMs of type `number` (optional) is the ping timeout in milliseconds for the provider.
  */
 export interface IClientConfig {
   providers: Array<IProvider>;
--- src/interfaces/IContractData.ts
+++ src/interfaces/IContractData.ts
@@ -4,6 +4,13 @@
  * This interface is used to track the smart contract information, including the
  * storage fee, maximum gas, contract data (as text or bytecode), contract address,
  * and an optional datastore for the smart contract's operational storage data.
+ *
+ * @see fee of type `bigint` represents the storage fee for the smart contract.
+ * @see maxGas of type `bigint` represents the maximum gas that can be consumed by the smart contract.
+ * @see contractDataText of type `string | undefined` represents the contract's data as text (optional).
+ * @see contractDataBinary of type `Uint8Array | undefined` represents the contract's data as bytecode (optional).
+ * @see address of type `string | undefined` represents the smart contract address (optional).
+ * @see datastore of type `Map<Uint8Array, Uint8Array> | undefined` represents the smart contract's operational storage data (optional).
  */
 export interface IContractData {
   fee: bigint;
