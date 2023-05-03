3ce1feebc6f944e3d4c8ae3f5280741f9f86124c
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-05-03 10:21:17 +0200

    Update bool.ts

--- src/utils/serializers/bool.ts
+++ src/utils/serializers/bool.ts
@@ -2,6 +2,7 @@
  * Converts a Uint8Array to boolean.
  *
  * @param arr - the Uint8Array to convert
+ * @return {boolean} the converted boolean
  */
 export function byteToBool(arr: Uint8Array): boolean {
   return !!arr[0];
@@ -11,6 +12,7 @@ export function byteToBool(arr: Uint8Array): boolean {
  * Converts a boolean to Uint8Array.
  *
  * @param val - the number to convert
+ * @return {Uint8Array} the converted Uint8Array
  */
 export function boolToByte(val: boolean): Uint8Array {
   return new Uint8Array([val ? 1 : 0]);
