b18b216bc25ff6d17f76a185f2f1396ecf2deee4
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-05-03 13:18:40 +0200

    Update unsigned.ts

--- src/utils/encode_decode_int/unsigned.ts
+++ src/utils/encode_decode_int/unsigned.ts
@@ -3,6 +3,16 @@ const LIMIT = BigInt(0x7f);
 const zeroBI = BigInt(0);
 const sevenBI = BigInt(7);
 
+/**
+ * Returns the number of bytes required to store the number in a varint.
+ *
+ * @privateRemarks
+ * If the v value is positive, it will be encoded as a positive value by multiplying it by 2.
+ * If the v value is negative, it will be encoded as a negative value by multiplying it by -2 and subtracting 1.
+ *
+ * @param value - The number to encode
+ * @returns The number of bytes required to store the number
+ */
 export function encodingLength(value: bigint): number {
   let i = 0;
 
@@ -13,6 +23,14 @@ export function encodingLength(value: bigint): number {
   return i + 1;
 }
 
+/**
+ * Encodes a big number as a varint.
+ *
+ * @param i - The big number to encode
+ * @param buffer - The buffer to write the varint to (optional)
+ * @param byteOffset - The offset in the buffer to start writing at (optional) (default: 0)
+ * @returns The buffer that was written to
+ */
 export function encode(
   i: bigint,
   buffer?: ArrayBuffer,
@@ -44,6 +62,13 @@ export function encode(
   return array;
 }
 
+/**
+ * Decodes a varint as a big number.
+ *
+ * @param data - The data to decode
+ * @param offset - The offset in the data to start reading at (optional) (default: 0)
+ * @returns The decoded big number
+ */
 export function decode(data: Uint8Array, offset = 0): bigint {
   let i = zeroBI;
   let n = 0;
