59c0f68e0d5104bdac3ab8a74b16508a49198f11
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-05-03 13:11:11 +0200

    Update signed.ts

--- src/utils/encode_decode_int/signed.ts
+++ src/utils/encode_decode_int/signed.ts
@@ -3,15 +3,40 @@ import * as unsigned from './unsigned.js';
 const oneBI = BigInt(1);
 const twoBI = BigInt(2);
 
+/**
+ * Encodes the length of the given value in bytes
+ *
+ * @remarks
+ * If the v value is positive, it will be encoded as a positive value by multiplying it by 2.
+ * If the v value is negative, it will be encoded as a negative value by multiplying it by -2 and subtracting 1.
+ *
+ * @param v - The value to encode
+ * @returns The length of the encoded value in bytes
+ */
 export function encodingLength(v: bigint) {
   return unsigned.encodingLength(v >= 0 ? v * twoBI : v * -twoBI - oneBI);
 }
 
+/**
+ * Encodes the given value into the given buffer at the given byteOffset
+ *
+ * @param v - The value to encode
+ * @param buffer - The buffer to encode into (optional)
+ * @param byteOffset - The byte offset to start encoding at (optional)
+ * @returns A buffer with the encoded value
+ */
 export function encode(v: bigint, buffer?: ArrayBuffer, byteOffset?: number) {
   v = v >= 0 ? v * twoBI : v * -twoBI - oneBI;
   return unsigned.encode(v, buffer, byteOffset);
 }
 
+/**
+ * Decodes the given data at the given byteOffset
+ *
+ * @param data - The data to decode
+ * @param offset - The byte offset to start decoding at (Default: 0)
+ * @returns The decoded value
+ */
 export function decode(data: Uint8Array, offset = 0) {
   const v = unsigned.decode(data, offset);
   return v & oneBI ? (v + oneBI) / -twoBI : v / twoBI;
