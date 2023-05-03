1b10400d39b43e6931dca1be53098fb1a10589f7
unknown <adriancik01@gmail.com>
2023-05-03 09:35:02 +0200

    improved index doc

--- src/utils/serializers/index.ts
+++ src/utils/serializers/index.ts
@@ -1,4 +1,25 @@
+/**
+ * This module exports functions for working with strings, including encoding and decoding
+ * between string and binary representations, such as Uint8Array.
+ */
 export * from './strings';
+
+/**
+ * This module exports functions for working with numeric data types, such as integers and
+ * floating-point numbers, and their binary representations in Uint8Array. The functions
+ * handle signed and unsigned integers, as well as BigInt for 64-bit integers.
+ */
 export * from './numbers';
+
+/**
+ * This module exports functions for working with boolean values, including functions for
+ * converting between boolean values and their binary representation in Uint8Array.
+ */
 export * from './bool';
+
+/**
+ * This module exports functions for working with arrays, including functions for manipulating
+ * and transforming arrays of various data types, such as integers, floating-point numbers,
+ * and other nested arrays.
+ */
 export * from './arrays';
