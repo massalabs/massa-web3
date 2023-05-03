c5b86655d3a4104b99832610efc9426f559a9f85
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-05-03 09:55:56 +0200

    Update Xbqcrypto.ts

--- src/utils/Xbqcrypto.ts
+++ src/utils/Xbqcrypto.ts
@@ -4,10 +4,22 @@ import { blake3 } from '@noble/hashes/blake3';
 import { signedBigIntUtils, unsignedBigIntUtils } from './encode_decode_int';
 const base58check = require('base58check');
 
+/**
+ * Hashes data with blake3
+ *
+ * @param data - The data to hash
+ * @returns The hash of the data with blake3
+ */
 export function hashBlake3(data: Uint8Array | string): Uint8Array {
   return blake3(data);
 }
 
+/**
+ * Encodes a buffer or an Uint8Array to base58
+ *
+ * @param data - The data to encode
+ * @returns The base58 encoded data as a string
+ */
 export function base58Encode(data: Buffer | Uint8Array): string {
   const bufData = Buffer.from(data);
   return base58check.encode(
@@ -16,11 +28,23 @@ export function base58Encode(data: Buffer | Uint8Array): string {
   );
 }
 
+/**
+ * Decode a base58 encoded string to a buffer
+ *
+ * @param data - The base58 encoded string
+ * @returns The decoded buffer
+ */
 export function base58Decode(data: string): Buffer {
   const decoded = base58check.decode(data);
   return Buffer.concat([decoded.prefix, decoded.data]);
 }
 
+/**
+ * The varint encoding is a variable length encoding of integers.
+ *
+ * @param data - The data to encode
+ * @returns The varint encoded data as a Uint8Array
+ */
 export function varintEncode(data: number | bigint): Uint8Array {
   if (typeof data === 'bigint') {
     return unsignedBigIntUtils.encode(data as bigint);
@@ -28,6 +52,12 @@ export function varintEncode(data: number | bigint): Uint8Array {
   return varint.encode(data);
 }
 
+/**
+ * Decodes a varint encoded Uint8Array to a number
+ *
+ * @param data
+ * @returns The decoded number
+ */
 export function varintDecode(data: Uint8Array): number {
   return varint.decode(data);
 }
