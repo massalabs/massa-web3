f1271241734a782892abb21c4556ca0992a24883
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-05-15 17:15:15 +0200

    update IWalletClient documentation

--- src/interfaces/IWalletClient.ts
+++ src/interfaces/IWalletClient.ts
@@ -24,24 +24,125 @@ import { ITransactionData } from './ITransactionData';
  * @see sellRolls - sell rolls
  */
 export interface IWalletClient {
+  /**
+   * Set base account for wallet
+   *
+   * @param baseAccount - base account as IAccount object
+   */
   setBaseAccount(baseAccount: IAccount): void;
+
+  /**
+   * Get the base account.
+   *
+   * @returns base account as IAccount object (or null if not set)
+   */
   getBaseAccount(): IAccount | null;
+
+  /**
+   * Get all accounts in wallet.
+   *
+   * @returns array of IAccount objects
+   */
   getWalletAccounts(): Array<IAccount>;
+
+  /**
+   * Delete all accounts from wallet.
+   */
   cleanWallet(): void;
+
+  /**
+   * Get an account by its address.
+   *
+   * @param address - address of the account
+   *
+   * @returns IAccount object or undefined if not found
+   */
   getWalletAccountByAddress(address: string): IAccount | undefined;
+
+  /**
+   * Add accounts to wallet by secret keys.
+   *
+   * @param secretKeys - array of secret keys
+   *
+   * @returns array of IAccount objects corresponding to the secret keys
+   */
   addSecretKeysToWallet(secretKeys: Array<string>): Promise<Array<IAccount>>;
+
+  /**
+   * Add accounts to wallet.
+   *
+   * @param accounts - array of IAccount objects
+   *
+   * @returns array of IAccount objects
+   */
   addAccountsToWallet(accounts: Array<IAccount>): Promise<Array<IAccount>>;
+
+  /**
+   * Remove accounts from wallet using their addresses.
+   *
+   * @param addresses - array of addresses
+   */
   removeAddressesFromWallet(addresses: Array<string>): void;
+
+  /**
+   * Get all accounts info.
+   *
+   * @returns array of IFullAddressInfo objects
+   */
   walletInfo(): Promise<Array<IFullAddressInfo>>;
+
+  /**
+   * Sign message using a specific account.
+   *
+   * @param data - message to sign
+   * @param accountSignerAddress - address of the account used to sign the message
+   *
+   * @returns the signed data as an ISignature object
+   */
   signMessage(
     data: string | Buffer,
     accountSignerAddress: string,
   ): Promise<ISignature>;
+
+  /**
+   * Get account balance.
+   *
+   * @param address - address of the account
+   *
+   * @returns IBalance object or null if not found
+   */
   getAccountBalance(address: string): Promise<IBalance | null>;
+
+  /**
+   * Send transaction.
+   *
+   * @param txData - transaction data
+   * @param executor - account used to send the transaction
+   *
+   * @returns array of operation ids
+   */
   sendTransaction(
     txData: ITransactionData,
     executor: IAccount,
   ): Promise<Array<string>>;
+
+  /**
+   * Buy rolls.
+   *
+   * @param txData - transaction data
+   * @param executor - account used to send the transaction
+   *
+   * @returns array of operation ids
+   */
   buyRolls(txData: IRollsData, executor: IAccount): Promise<Array<string>>;
+
+  /**
+   * Sell rolls.
+   *
+   * @param txData - transaction data
+   * @param executor - account used to send the transaction
+   *
+   * @returns array of operation ids
+   */
   sellRolls(txData: IRollsData, executor: IAccount): Promise<Array<string>>;
 }
