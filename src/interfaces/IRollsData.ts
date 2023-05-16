308513afebf71a80b669502979a36cf9ffd57726
Ben <br@massa.net>
2023-05-16 10:52:50 +0200

    [fix] rolls account doc

--- src/interfaces/IRollsData.ts
+++ src/interfaces/IRollsData.ts
@@ -2,7 +2,7 @@
  * Represents the data of a roll.
  *
  * @see fee - fees paid for the roll
- * @see amount - amount paid for the roll
+ * @see amount - amount of rolls
  */
 export interface IRollsData {
   fee: bigint;
