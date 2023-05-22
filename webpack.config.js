f27aa8819c481945e2699cadf86079d2e1c6055c
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-05-22 11:08:00 +0200

    update webpackconfig, removing useless packages

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,24 @@ const path = require('path');
 
 // Export the webpack configuration object
 module.exports = [
-  // UMD
+  // UMD minified (browser only)
   {
     // The entry point of the package
     entry: './src/index.ts',
 
-    experiments: {
-      outputModule: true, // This line is required for ESM output
-    },
-
     // Configuration for the output file
     output: {
       // The filename of the output file
-      filename: 'massa-web3.min.js',
+      filename: 'bundle.js',
 
       // The path to the output directory, __dirname is the directory of the current module
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, '.'),
 
       // The type of the exported library
-      libraryTarget: 'module',
+      libraryTarget: 'window', // for UMD we use window
+
+      // The name of the library as it should be exposed in the global scope
+      library: 'massa',
 
       // The global object in which the library will be assigned to
       globalObject: 'this',
@@ -31,16 +30,9 @@ module.exports = [
     resolve: {
       // Configure fallbacks for Node.js built-in modules
       fallback: {
-        // Fallback for the crypto module, using crypto-browserify
-        crypto: require.resolve('crypto-browserify/'),
-
-        // Fallback for the stream module, using stream-browserify
-        stream: require.resolve('stream-browserify/'),
-
         // Fallback for the buffer module, using the buffer module
         buffer: require.resolve('buffer/'),
       },
-
       // Extensions that are used to resolve modules
       extensions: ['.ts', '.js'],
     },
@@ -75,5 +67,72 @@ module.exports = [
     // The mode to use for the webpack build
     mode: 'development', // In development mode, the configuration enables NamedChunksPlugin and NamedModulesPlugin.
     // You may want to switch to 'production' for deployment.
+  },
+
+  // UMD - non-minified (browser only)
+  {
+    // Same as above but with optimization.minimize set to false,
+    // The entry point of the package
+    entry: './src/index.ts',
+
+    // Configuration for the output file
+    output: {
+      // The filename of the output file
+      filename: 'bundle.non-minified.js',
+
+      // The path to the output directory, __dirname is the directory of the current module
+      path: path.resolve(__dirname, '.'),
+
+      // The type of the exported library
+      libraryTarget: 'window', // for UMD we use window
+
+      // The name of the library as it should be exposed in the global scope
+      library: 'massa',
+
+      // The global object in which the library will be assigned to
+      globalObject: 'this',
+    },
+
+    // Configuration for module resolution
+    resolve: {
+      // Configure fallbacks for Node.js built-in modules
+      fallback: {
+        // Fallback for the buffer module, using the buffer module
+        buffer: require.resolve('buffer/'),
+      },
+
+      // Extensions that are used to resolve modules
+      extensions: ['.ts', '.js'],
+    },
+
+    // Configuration for modules
+    module: {
+      // Array of rules that are used to find and load modules
+      rules: [
+        {
+          // Regular expression that matches the file extensions that this rule applies to
+          test: /\.ts$/,
+          // The loader that should be used for the files that match the test regular expression
+          loader: 'ts-loader',
+          // A condition that must not be met to use this rule
+          exclude: /node_modules/,
+          options: {
+            configFile: 'tsconfig.commonjs.json',
+          },
+        },
+      ],
+    },
+
+    // Configuration for optimization
+    optimization: {
+      // Whether to minimize the output. Set to false for the non-minified bundle.
+      minimize: false,
+    },
+
+    // Configuration for source maps
+    devtool: 'source-map',
+
+    // The mode to use for the webpack build. Set to 'development' for the non-minified bundle.
+    mode: 'development',
   },
 ];
