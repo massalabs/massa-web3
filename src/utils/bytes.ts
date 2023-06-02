632d94139f72de0d624baec7df913b0df97086fd
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-06-02 17:19:58 +0200

    fix secretKey constructor

--- src/utils/bytes.ts
+++ src/utils/bytes.ts
@@ -4,7 +4,7 @@ const SECRET_KEY_PREFIX = 'S';
 const PUBLIC_KEY_PREFIX = 'P';
 
 /**
- * Get the byte representation of a given secret key.
+ * Get the byte representation of a given secret key, without the version.
  *
  * @param secretKey - The secret key to get the bytes from.
  *
@@ -13,6 +13,29 @@ const PUBLIC_KEY_PREFIX = 'P';
  * @returns a Uint8Array containing the bytes of the secret key.
  */
 export function getBytesSecretKey(secretKey: string): Uint8Array {
+  if (!(secretKey[0] == SECRET_KEY_PREFIX)) {
+    throw new Error(
+      `Invalid secret key prefix: "${secretKey[0]}". The secret key should start with "${SECRET_KEY_PREFIX}". Please verify your secret key and try again.`,
+    );
+  }
+  const secretKeyVersionBase58Decoded: Buffer = base58Decode(
+    secretKey.slice(1),
+  );
+  // Version is little for now.
+  const secretKeyBase58Decoded = secretKeyVersionBase58Decoded.slice(1);
+  return secretKeyBase58Decoded;
+}
+
+/**
+ * Get the byte representation of a given secret key.
+ *
+ * @param secretKey - The secret key to get the bytes from.
+ *
+ * @throws if the secret key is not valid.
+ *
+ * @returns a Uint8Array containing the bytes of the secret key.
+ */
+export function getBytesSecretKeyVersioned(secretKey: string): Uint8Array {
   if (!(secretKey[0] == SECRET_KEY_PREFIX)) {
     throw new Error(
       `Invalid secret key prefix: "${secretKey[0]}". The secret key should start with "${SECRET_KEY_PREFIX}". Please verify your secret key and try again.`,
