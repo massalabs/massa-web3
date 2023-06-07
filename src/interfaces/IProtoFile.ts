7a8a2964f6e096857809c932096098e903247c67
Elli610 <113121626+Elli610@users.noreply.github.com>
2023-06-07 09:58:44 +0200

    add the "getProtoFiles" to the SmartContractsClient class

---
+++ src/interfaces/IProtoFile.ts
@@ -0,0 +1,11 @@
+/**
+ * This interface is used to represent proto files retrieved from smart contracts.
+ *
+ * @see final_value - The current version of the proto file as a byte array
+ * @see candidate_value - If it is different from the final_value, it represents the
+ * candidate file (as a byte array) to replace the current proto file.
+ */
+export interface IProtoFile {
+  final_value: string;
+  candidate_value: string;
+}
