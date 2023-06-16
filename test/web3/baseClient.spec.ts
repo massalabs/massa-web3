d8d54ac8418722eac7faa7f38be1d8cd217d4409
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-06-16 12:19:59 +0200

    new tests with private functions

--- test/web3/baseClient.spec.ts
+++ test/web3/baseClient.spec.ts
@@ -3,34 +3,47 @@ import { IProvider, ProviderType } from '../../src/interfaces/IProvider';
 import { IClientConfig } from '../../src/interfaces/IClientConfig';
 import { JSON_RPC_REQUEST_METHOD } from '../../src/interfaces/JsonRpcMethods';
 import nock from 'nock';
+import axios, { AxiosRequestHeaders } from 'axios';
+
+jest.mock('axios');
 
 // for CI testing:
-const publicApi = 'https://test.massa.net/api/v2:33035';
-const privateApi = 'https://test.massa.net/api/v2:33034';
+// const publicApi = 'http://mock-public-api.com';
+// const privateApi = 'http://mock-private-api.com';
 
 // For local testing:
-// const publicApi = 'http://127.0.0.1:33035';
-// const privateApi = 'http://127.0.0.1:33034';
+const publicApi = 'http://127.0.0.1:33035';
+const privateApi = 'http://127.0.0.1:33034';
 
 export const PERIOD_OFFSET = 5;
+
+const requestHeaders = {
+  Accept:
+    'application/json,text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true,
+  'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+} as AxiosRequestHeaders;
 
 class TestBaseClient extends BaseClient {
   constructor(config: IClientConfig) {
     super(config);
   }
 
-  public getPrivateProviders() {
-    return super.getPrivateProviders();
-  }
-
-  public getPublicProviders() {
-    return super.getPublicProviders();
+  public getSendJsonRPCRequest(
+    method: JSON_RPC_REQUEST_METHOD,
+    params: object,
+  ) {
+    return this.sendJsonRPCRequest(method, params);
   }
 }
 
 describe('BaseClient', () => {
   describe('getProviderForRpcMethod', () => {
-    test('getProviderForRpcMethod should return a public provider for public RPC methods', () => {
+    let baseClient: TestBaseClient;
+    const mockAxios = axios as jest.Mocked<typeof axios>;
+
+    beforeEach(() => {
       const clientConfig: IClientConfig = {
         providers: [
           { url: publicApi, type: ProviderType.PUBLIC } as IProvider,
@@ -39,54 +52,105 @@ describe('BaseClient', () => {
         periodOffset: PERIOD_OFFSET,
       };
 
-      const baseClient = new BaseClient(clientConfig);
-      const provider = baseClient.getProviderForRpcMethod(
-        JSON_RPC_REQUEST_METHOD.GET_ADDRESSES,
-      );
-
-      expect(provider.url).toBe(publicApi);
-      expect(provider.type).toBe(ProviderType.PUBLIC);
+      baseClient = new TestBaseClient(clientConfig);
+      jest.resetAllMocks();
     });
 
-    test('getProviderForRpcMethod should return a private provider for private RPC methods', () => {
-      const clientConfig: IClientConfig = {
-        providers: [
-          { url: publicApi, type: ProviderType.PUBLIC } as IProvider,
-          { url: privateApi, type: ProviderType.PRIVATE } as IProvider,
-        ],
-        periodOffset: PERIOD_OFFSET,
+    test('should use public provider for public RPC methods', async () => {
+      const mockResponse = {
+        data: {
+          result: {
+            error: null,
+            isError: false,
+            result: 'some data returned from the API',
+          },
+          error: null,
+          isError: false,
+        },
       };
 
-      const baseClient = new BaseClient(clientConfig);
-      const provider = baseClient.getProviderForRpcMethod(
-        JSON_RPC_REQUEST_METHOD.STOP_NODE,
+      mockAxios.post.mockResolvedValueOnce(mockResponse);
+
+      const result = await baseClient.getSendJsonRPCRequest(
+        JSON_RPC_REQUEST_METHOD.GET_STATUS, // public method
+        {},
       );
 
-      expect(provider.url).toBe(privateApi);
-      expect(provider.type).toBe(ProviderType.PRIVATE);
+      expect(mockAxios.post).toHaveBeenCalledTimes(1);
+      expect(mockAxios.post).toHaveBeenCalledWith(
+        publicApi, // <-- We check here that the public API is used
+        {
+          jsonrpc: '2.0',
+          id: 0,
+          method: JSON_RPC_REQUEST_METHOD.GET_STATUS,
+          params: {},
+        },
+        requestHeaders,
+      );
+
+      expect(result).toEqual(mockResponse.data.result);
     });
 
-    test('getProviderForRpcMethod should throw an error for unknown RPC methods', () => {
-      const clientConfig: IClientConfig = {
-        providers: [
-          { url: publicApi, type: ProviderType.PUBLIC } as IProvider,
-          { url: privateApi, type: ProviderType.PRIVATE } as IProvider,
-        ],
-        periodOffset: PERIOD_OFFSET,
+    test('should use private provider for private RPC methods', async () => {
+      const mockResponse = {
+        data: {
+          result: {
+            error: null,
+            isError: false,
+            result: 'some data returned from the API',
+          },
+          error: null,
+          isError: false,
+        },
       };
 
-      const baseClient = new BaseClient(clientConfig);
+      mockAxios.post.mockResolvedValueOnce(mockResponse);
 
-      expect(() => {
-        baseClient.getProviderForRpcMethod(
-          'UNKNOWN_METHOD' as JSON_RPC_REQUEST_METHOD,
-        );
-      }).toThrow(Error);
+      const result = await baseClient.getSendJsonRPCRequest(
+        JSON_RPC_REQUEST_METHOD.STOP_NODE, // private method
+        {},
+      );
+
+      expect(mockAxios.post).toHaveBeenCalledTimes(1);
+      expect(mockAxios.post).toHaveBeenCalledWith(
+        privateApi, // <-- We check here that the private API is used
+        {
+          jsonrpc: '2.0',
+          id: 0,
+          method: JSON_RPC_REQUEST_METHOD.STOP_NODE,
+          params: {},
+        },
+        requestHeaders,
+      );
+
+      expect(result).toEqual(mockResponse.data.result);
+    });
+
+    test('getProviderForRpcMethod should throw an error for unknown RPC methods', async () => {
+      const mockResponse = {
+        data: {
+          result: {
+            error: null,
+            isError: false,
+            result: 'some data returned from the API',
+          },
+          error: null,
+          isError: false,
+        },
+      };
+
+      mockAxios.post.mockResolvedValueOnce(mockResponse);
+
+      const unknownMethod = 'unknownMethod' as JSON_RPC_REQUEST_METHOD;
+
+      await expect(
+        baseClient.getSendJsonRPCRequest(unknownMethod, {}),
+      ).rejects.toThrowError(`Unknown Json rpc method: ${unknownMethod}`);
     });
   });
 
   describe('promisifyJsonRpcCall', () => {
-    let baseClient: BaseClient;
+    let baseClient: TestBaseClient;
     const clientConfig: IClientConfig = {
       providers: [
         {
@@ -102,7 +166,7 @@ describe('BaseClient', () => {
     };
 
     beforeEach(() => {
-      baseClient = new BaseClient(clientConfig);
+      baseClient = new TestBaseClient(clientConfig);
     });
 
     afterEach(() => {
