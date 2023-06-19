3168478d44b7877861977758be5b4b22267b41d4
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-06-19 10:41:35 +0200

    customClientFactory

--- test/web3/clientFactory.spec.ts
+++ test/web3/clientFactory.spec.ts
@@ -3,8 +3,12 @@ import {
   DefaultProviderUrls,
 } from '../../src/web3/ClientFactory';
 import { WalletClient } from '../../src/web3/WalletClient';
+import { ProviderType } from '../../src/interfaces/IProvider';
 import { Client } from '../../src/web3/Client';
 import { IAccount } from '../../src/interfaces/IAccount';
+
+const publicApi = 'http://mock-public-api.com';
+const privateApi = 'http://mock-private-api.com';
 
 describe('ClientFactory', () => {
   describe('createDefaultClient', () => {
@@ -36,6 +40,41 @@ describe('ClientFactory', () => {
 
       expect(client).toBeInstanceOf(Client);
       expect(client.getProviders()).toHaveLength(2);
+      expect(client.wallet().getBaseAccount()).toBeUndefined();
+    });
+  });
+
+  describe('createCustomClient', () => {
+    test('should create a custom client with the specified providers', async () => {
+      const providers = [
+        { url: publicApi, type: ProviderType.PUBLIC },
+        { url: privateApi, type: ProviderType.PRIVATE },
+      ];
+      const baseAccount: IAccount =
+        await WalletClient.walletGenerateNewAccount();
+
+      const client = await ClientFactory.createCustomClient(
+        providers,
+        true,
+        baseAccount,
+      );
+
+      expect(client).toBeInstanceOf(Client);
+      expect(client.getProviders()).toHaveLength(2);
+      expect(client.getProviders()).toEqual(providers);
+    });
+
+    test('should create a custom client without a base account if not provided', async () => {
+      const providers = [
+        { url: publicApi, type: ProviderType.PUBLIC },
+        { url: privateApi, type: ProviderType.PRIVATE },
+      ];
+
+      const client = await ClientFactory.createCustomClient(providers, true);
+
+      expect(client).toBeInstanceOf(Client);
+      expect(client.getProviders()).toHaveLength(2);
+      expect(client.getProviders()).toEqual(providers);
       expect(client.wallet().getBaseAccount()).toBeUndefined();
     });
   });
