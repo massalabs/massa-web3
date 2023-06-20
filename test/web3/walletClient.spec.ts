1a382ccb96f3566c685e7f2ab35cd725f0ee7516
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-06-20 16:18:01 +0200

    first 2 tests sendTransaction

--- test/web3/walletClient.spec.ts
+++ test/web3/walletClient.spec.ts
@@ -7,6 +7,7 @@ import { IProvider, ProviderType } from '../../src/interfaces/IProvider';
 import { expect, test, describe, beforeEach, afterEach } from '@jest/globals';
 import * as ed from '@noble/ed25519';
 import { ISignature } from '../../src/interfaces/ISignature';
+import { ITransactionData } from '../../src/interfaces/ITransactionData';
 
 // TODO: Use env variables and say it in the CONTRIBUTING.md
 const deployerPrivateKey =
@@ -588,5 +589,75 @@ describe('WalletClient', () => {
 
       expect(balance).toBeNull();
     });
+  });
+
+  describe.only('sendTransaction', () => {
+    let receiverAccount: IAccount;
+    let mockTxData: ITransactionData;
+
+    beforeEach(async () => {
+      receiverAccount = await WalletClient.walletGenerateNewAccount();
+      mockTxData = {
+        fee: 1n,
+        amount: 100n,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        recipientAddress: receiverAccount.address!,
+      };
+    });
+
+    test('should throw an error if no sender account is available for the transaction', async () => {
+      jest.spyOn(web3Client.wallet(), 'getBaseAccount').mockReturnValue(null);
+
+      await expect(
+        web3Client.wallet().sendTransaction(mockTxData),
+      ).rejects.toThrow('No tx sender available');
+    });
+
+    // test('should call compactBytesForOperation with correct arguments', async () => {
+
+    //   const spy = jest.spyOn(web3Client.wallet(), 'compactBytesForOperation');
+
+    //   await web3Client.wallet().sendTransaction(mockTxData);
+
+    //   expect(spy).toHaveBeenCalledWith(
+    //     mockTxData,
+    //     OperationTypeId.Transaction,
+    //     expect.any(Number), // expiryPeriod
+    //   );
+    // });
+
+    test('should call walletSignMessage with correct arguments', async () => {
+      const spy = jest.spyOn(WalletClient, 'walletSignMessage');
+
+      await web3Client.wallet().sendTransaction(mockTxData);
+
+      expect(spy).toHaveBeenCalledWith(
+        expect.any(Buffer), // Buffer.concat([bytesPublicKey, bytesCompact])
+        expect.any(Object), // sender
+      );
+    });
+
+    // test('should call sendJsonRPCRequest with correct arguments', async () => {
+    //   const spy = jest.spyOn(web3Client.wallet(), 'sendJsonRPCRequest');
+
+    //   await web3Client.wallet().sendTransaction(mockTxData);
+
+    //   expect(spy).toHaveBeenCalledWith(
+    //     JSON_RPC_REQUEST_METHOD.SEND_OPERATIONS,
+    //     expect.any(Array), // [[data]]
+    //   );
+    // });
+
+    // test('should return an array of operation ids', async () => {
+    //   const mockOpIds = ['op1', 'op2', 'op3'];
+
+    //   jest
+    //     .spyOn(web3Client.wallet(), 'sendJsonRPCRequest')
+    //     .mockResolvedValue(mockOpIds);
+
+    //   const opIds = await web3Client.wallet().sendTransaction(mockTxData);
+
+    //   expect(opIds).toEqual(mockOpIds);
+    // });
   });
 });
