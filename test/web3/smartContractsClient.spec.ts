a4ce92de14cafa3341a9a87ce3b19f406c5adb60
julienbrs <106234742+julienbrs@users.noreply.github.com>
2023-06-22 15:53:20 +0200

    solve conflicts

--- test/web3/smartContractsClient.spec.ts
+++ test/web3/smartContractsClient.spec.ts
@@ -6,9 +6,10 @@ import { WalletClient } from '../../src/web3/WalletClient';
 import {
   mockClientConfig,
   mockDeployerAccount,
+  mockCallData,
+  mockContractData,
   mockNodeStatusInfo,
   mockOpIds,
-  mockCallData,
 } from './mockData';
 
 describe('SmartContractsClient', () => {
@@ -46,6 +47,102 @@ describe('SmartContractsClient', () => {
     (smartContractsClient as any).sendJsonRPCRequest = jest
       .fn()
       .mockResolvedValue(mockOpIds);
+  });
+
+  describe('deploySmartContract', () => {
+    test('should call sendJsonRPCRequest with correct arguments', async () => {
+      await smartContractsClient.deploySmartContract(
+        mockContractData,
+        mockDeployerAccount,
+      );
+
+      expect(
+        (smartContractsClient as any).sendJsonRPCRequest,
+      ).toHaveBeenCalledWith(JSON_RPC_REQUEST_METHOD.SEND_OPERATIONS, [
+        [
+          {
+            serialized_content: expect.any(Array),
+            creator_public_key: mockDeployerAccount.publicKey,
+            signature: 'signature',
+          },
+        ],
+      ]);
+    });
+
+    test('should return the correct result', async () => {
+      const result = await smartContractsClient.deploySmartContract(
+        mockContractData,
+      );
+
+      expect(result).toBe(mockOpIds[0]);
+    });
+
+    // Write additional tests to handle any edge cases or error scenarios
+    test('should handle errors correctly', async () => {
+      const mockError = new Error('Error message');
+      (smartContractsClient as any).sendJsonRPCRequest = jest
+        .fn()
+        .mockRejectedValue(mockError);
+
+      await expect(
+        smartContractsClient.deploySmartContract(mockContractData),
+      ).rejects.toThrow(mockError);
+    });
+
+    test('should throw error when no executor is provided and base account is not set', async () => {
+      mockWalletClient.getBaseAccount = jest.fn().mockReturnValue(null);
+      await expect(
+        smartContractsClient.deploySmartContract(mockContractData),
+      ).rejects.toThrow(`No tx sender available`);
+    });
+
+    test('should use default account when no executor is provided', async () => {
+      await smartContractsClient.deploySmartContract(mockContractData);
+      expect(mockWalletClient.getBaseAccount).toHaveBeenCalled();
+    });
+
+    test('should warn when contractDataBinary size exceeded half of the maximum size of a block', async () => {
+      const max_block_size = mockNodeStatusInfo.config.max_block_size;
+      mockContractData.contractDataBinary = new Uint8Array(
+        max_block_size / 2 + 1,
+      ); // value > max_block_size / 2
+
+      const consoleWarnSpy = jest.spyOn(console, 'warn');
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      consoleWarnSpy.mockImplementation(() => {});
+
+      await smartContractsClient.deploySmartContract(mockContractData);
+
+      expect(consoleWarnSpy).toHaveBeenCalledWith(
+        'bytecode size exceeded half of the maximum size of a block, operation will certainly be rejected',
+      );
+
+      // Restore console.warn
+      consoleWarnSpy.mockRestore();
+    });
+
+    test('should throw error when contractDataBinary does not exist', async () => {
+      const modifiedMockContractData = { ...mockContractData };
+      delete modifiedMockContractData.contractDataBinary;
+
+      await expect(
+        smartContractsClient.deploySmartContract(modifiedMockContractData),
+      ).rejects.toThrow(
+        `Expected non-null contract bytecode, but received null.`,
+      );
+    });
+
+    test('should throw error when no opId is returned', async () => {
+      (smartContractsClient as any).sendJsonRPCRequest = jest
+        .fn()
+        .mockResolvedValue([]);
+
+      await expect(
+        smartContractsClient.deploySmartContract(mockContractData),
+      ).rejects.toThrow(
+        `Deploy smart contract operation bad response. No results array in json rpc response. Inspect smart contract`,
+      );
+    });
   });
 
   describe('callSmartContract', () => {
