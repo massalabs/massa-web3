9cfc1ba783c7dc55b46744f88fcf924832b8dc4d
Pierre Seznec <34547263+peterjah@users.noreply.github.com>
2023-06-22 17:30:41 +0200

    ci: support publish for multiple networks (#326)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,10 +3,6 @@ set -e
 
 npm ci
 npm run build
-npm version --preid dev --no-git-tag-version --no-commit-hooks prepatch
-#Use timestamp as package suffix
-TIME=$(date -u +%Y%m%d%H%M%S)
-sed -i "/version/s/dev.0/dev.$TIME/g" package.json
 
 PACKAGE_NAME=$(cat package.json | jq -r '.name')
 PUBLISH_VERSION=$(cat package.json | jq -r '.version')
@@ -15,7 +11,7 @@ echo publishing ${PACKAGE_NAME}@$PUBLISH_VERSION
 BRANCH=${GITHUB_REF##*/}
 TAG=""
 if [[ "$BRANCH" == "buildnet" ]]; then
-  TAG="buildnet-"
+  TAG="--tag buildnet"
 fi
 
-npm publish --access public --tag ${TAG}dev
+npm publish --access public $TAG
