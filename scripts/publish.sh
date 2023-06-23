d74c11d38c55581a232ae8f667b6b0774360e459
Senji888 <44082144+Ben-Rey@users.noreply.github.com>
2023-06-23 12:44:14 +0200

    fix ci missing branch name (#340)

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,9 +6,9 @@ npm run build
 
 PACKAGE_NAME=$(cat package.json | jq -r '.name')
 PUBLISH_VERSION=$(cat package.json | jq -r '.version')
-echo publishing ${PACKAGE_NAME}@$PUBLISH_VERSION
+echo "Publishing ${PACKAGE_NAME}@$PUBLISH_VERSION"
 
-BRANCH=${GITHUB_REF##*/}
+BRANCH=$1
 TAG=""
 if [[ "$BRANCH" == "buildnet" ]]; then
   TAG="--tag buildnet"
