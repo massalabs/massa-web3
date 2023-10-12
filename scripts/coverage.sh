b59cad333ec2186c22d5e0c7ea8be2f806341d24
Ben <br@massa.net>
2023-10-12 12:59:08 +0200

    Update coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Running coverage script..."
-echo $COVERAGE
-echo $PROJECT
+echo "Coverage: $COVERAGE"
+echo "Project: $PROJECT"
 
 # Define the color based on coverage threshold
 determine_color() {
@@ -19,25 +19,10 @@ determine_color() {
 
 filename="README.md"
 
-# Indexed arrays for projects and their corresponding line numbers
-projects=("massa-web3" "web3-utils")
-lines=(9 18)
+declare -A project_map
+project_map=( ["massa-web3"]=9 ["web3-utils"]=18 ) 
 
-# Get line number for the given project
-get_line_number() {
-    local project=$1
-    local i=0
-    for p in "${projects[@]}"; do
-        if [ "$p" == "$project" ]; then
-            echo "${lines[$i]}"
-            return
-        fi
-        ((i++))
-    done
-    echo ""
-}
-
-line_number=$(get_line_number $PROJECT)
+line_number=${project_map[$PROJECT]}
 
 # If no line number found, exit the script
 if [ -z "$line_number" ]; then
@@ -45,32 +30,36 @@ if [ -z "$line_number" ]; then
     exit 1
 fi
 
-# If no line number found, exit the script
-if [ -z "$line_number" ]; then
-    echo "Project '$PROJECT' not found in the map."
-    exit 1
-fi
-
+echo "Line number: $line_number"
 # Extract the current coverage badge from the specified line
 coverageLine=$(sed -n "${line_number}p" $filename)
 
 # Regex to match the coverage percentage
-regex="${PROJECT}:\s*([0-9]+([.][0-9]+)?)%"
+regex="!\[check-code-coverage\]\(https://img.shields.io/badge/coverage-([0-9]+([.][0-9]+)?)%25-[^)]+\)"
+
+echo "Coverage line: $coverageLine"
+
 
 if [[ $coverageLine =~ $regex ]]; then
     oldCoverage="${BASH_REMATCH[1]}"
-    echo "Coverage for $PROJECT is $COVERAGE%"
+    echo "Coverage for $PROJECT is $oldCoverage%"
 else
     echo "No coverage found for $PROJECT."
+    exit 1
 fi
 
 # Determine the color for the badge based on the coverage
 color=$(determine_color $COVERAGE)
 
 # Update the coverage badge if the difference in coverage is greater than or equal to 1%
-if [ -z "$oldCoverage" ] || (( $(echo "$COVERAGE - $oldCoverage >= 1" | bc -l) )) || (( $(echo "$oldCoverage - $COVERAGE >= 1" | bc -l) )); then
+if (( $(echo "$COVERAGE != $oldCoverage" | bc -l) )); then
     echo "Updating badge for $PROJECT."
     newLine="![check-code-coverage](https://img.shields.io/badge/coverage-$COVERAGE%25-$color)"
     sed -i "${line_number}s#.*#${newLine}#" $filename
-
+    echo "Updated badge for $PROJECT."
+    coverageLine=$(sed -n "${line_number}p" $filename)
+    
+    echo "Coverage line: $coverageLine"
+else
+    echo "Coverage for $PROJECT has not changed. Skipping update."
 fi
