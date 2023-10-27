82df62393bcae5674c280cd0291746f4e45140e5
Senji888 <44082144+Ben-Rey@users.noreply.github.com>
2023-10-27 17:22:15 +0200

    Fix publish-dev.sh script (#490)

--- scripts/publish-dev.sh
+++ scripts/publish-dev.sh
@@ -6,11 +6,7 @@ npm run build
 npm version --preid dev --no-git-tag-version --no-commit-hooks prepatch
 #Use timestamp as package suffix
 TIME=$(date -u +%Y%m%d%H%M%S)
-sed -i "/version/s/dev.0/dev.$TIME/g" package.json
-
-PACKAGE_NAME=$(cat package.json | jq -r '.name')
-PUBLISH_VERSION=$(cat package.json | jq -r '.version')
-echo publishing ${PACKAGE_NAME}@$PUBLISH_VERSION
+sed -i "/version/s/dev.0/dev.$TIME/g" packages/*/package.json
 
 BRANCH=${GITHUB_REF##*/}
 TAG=""
@@ -20,4 +16,4 @@ elif [[ "$BRANCH" == "testnet" ]]; then
   TAG="testnet-"
 fi
 
-npm publish --access public --tag ${TAG}dev
+npm publish --ws --access public --tag ${TAG}dev
