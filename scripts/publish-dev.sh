6e5f4404e6f10ccaefc61b142755d24afa45b68f
Senji888 <44082144+Ben-Rey@users.noreply.github.com>
2023-11-03 10:34:03 +0100

    Fix publish-dev.sh script

--- scripts/publish-dev.sh
+++ scripts/publish-dev.sh
@@ -3,7 +3,7 @@ set -e
 
 npm ci
 npm run build
-npm version --preid dev --no-git-tag-version --no-commit-hooks prepatch
+npm version --workspaces --preid dev --no-git-tag-version --no-commit-hooks prepatch
 #Use timestamp as package suffix
 TIME=$(date -u +%Y%m%d%H%M%S)
 sed -i "/version/s/dev.0/dev.$TIME/g" packages/*/package.json
