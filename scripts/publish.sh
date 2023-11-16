65369582abe5678b57c48b42e20062cc332b8c35
Senji888 <44082144+Ben-Rey@users.noreply.github.com>
2023-11-16 17:12:59 +0100

    Update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,45 +1,14 @@
 #!/bin/bash
-set -e
+set -ex
+# cd ./packages/sc-deployer
+# npm publish --access public
+# npm publish "@massalabs/massa-proto-cli"
+# npm publish "@massalabs/massa-sc-deployer" 
 
-# Check if Node.js and npm are installed
-if ! [ -x "$(command -v node)" ] || ! [ -x "$(command -v npm)" ]; then
-  echo "Node.js and npm are required to run this script."
-  exit 1
-fi
+npm dist-tag add "@massalabs/massa-web3@2.2.6" buildnet
+# npm dist-tag add "@massalabs/massa-web3@2.2.2" latest
+# npm dist-tag add "@massalabs/massa-sc-deployer@1.1.1" latest
 
-npm ci
-npm run build
+# npm dist-tag rm "@massalabs/sc-project-initializer@0.5.0" testnet
+# npm dist-tag rm "@massalabs/massa-sc-deployer@0.4.0" testnet
 
-ref=$1
-echo "Publishing packages for reference: $ref"
-TAG=""
-
-if [[ "$ref" == *"buildnet"* ]]; then
-  TAG="--tag buildnet"
-fi
-
-for packageDir in packages/*; do
-  if [ -d "$packageDir" ]; then
-    PACKAGE_NAME=$(cat "$packageDir/package.json" | jq -r '.name')
-    PUBLISH_VERSION=$(cat "$packageDir/package.json" | jq -r '.version')
-
-    # Check if the package name is in the reference
-    if [[ "$ref" == *"$PACKAGE_NAME"* ]]; then
-      # Check if the package version is already published
-      if [[ "$ref" == "buildnet" ]]; then
-        CURRENT_VERSION=$(npm view "$PACKAGE_NAME@buildnet" version 2>/dev/null || true)
-      else
-        CURRENT_VERSION=$(npm view "$PACKAGE_NAME" version 2>/dev/null || true)
-      fi
-
-      if [ "$CURRENT_VERSION" == "$PUBLISH_VERSION" ]; then
-        echo "${PACKAGE_NAME}@${PUBLISH_VERSION} is already published. Skipping."
-      else
-        echo "Publishing ${PACKAGE_NAME}@${PUBLISH_VERSION}"
-        npm publish --workspace="$PACKAGE_NAME" "$TAG"
-      fi
-    else
-      echo "Skipping ${PACKAGE_NAME}"
-    fi
-  fi
-done
