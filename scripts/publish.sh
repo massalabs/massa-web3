5cbab69c7decd9695866dfa67300c09fa0e2d393
Senji888 <44082144+Ben-Rey@users.noreply.github.com>
2023-11-22 14:27:36 +0100

    Bump

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -ex
-# cd ./packages/sc-deployer
+cd ./packages/massa-web3
 # npm publish --access public
 # npm publish "@massalabs/massa-proto-cli"
-# npm publish "@massalabs/massa-sc-deployer" 
+npm publish "@massalabs/massa-web3" 
 
 npm dist-tag add "@massalabs/massa-web3@2.2.6" buildnet
-
-# npm dist-tag add "@massalabs/massa-web3@1.24.4" buildnet
-# npm dist-tag add "@massalabs/massa-sc-deployer@1.1.1" latest
 
 # npm dist-tag rm "@massalabs/sc-project-initializer@0.5.0" testnet
 # npm dist-tag rm "@massalabs/massa-sc-deployer@0.4.0" testnet
