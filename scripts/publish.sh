e42a76d0609d7dddc94d2f6ad73d3e78424942c4
Senji888 <44082144+Ben-Rey@users.noreply.github.com>
2023-11-22 15:10:10 +0100

    undo

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
-cd ./packages/massa-web3
+# cd ./packages/massa-web3
 # npm publish --access public
 # npm publish "@massalabs/massa-proto-cli"
-npm publish "@massalabs/massa-web3" 
 
 npm dist-tag add "@massalabs/massa-web3@2.2.6" buildnet
+
 
 # npm dist-tag rm "@massalabs/sc-project-initializer@0.5.0" testnet
 # npm dist-tag rm "@massalabs/massa-sc-deployer@0.4.0" testnet
