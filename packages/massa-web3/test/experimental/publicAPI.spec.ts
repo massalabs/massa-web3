e9564006ffbed5cae1b70075c29e3fddcb4432e3
Grégory Libert <gl@massa.net>
2024-04-05 18:08:33 +0200

    movefrom check to strictCheck to identify extraneous values

--- packages/massa-web3/test/experimental/publicAPI.spec.ts
+++ packages/massa-web3/test/experimental/publicAPI.spec.ts
@@ -26,36 +26,36 @@ describe('unit tests', () => {
     const status = await api.getStatus()
     lastSlot.period = status.last_slot!.period
     lastSlot.thread = status.last_slot!.thread
-    NodeStatus.check(status)
+    NodeStatus.strictCheck(status)
   })
 
   test('getAddressInfo', async () => {
     const info = await api.getAddressInfo(
       'AU1Fp7uBP2TXxDty2HdTE3ZE3XQ4cNXnG3xuo8TkQLJtyxC7FKhx'
     )
-    AddressInfo.check(info)
+    AddressInfo.strictCheck(info)
   })
 
   test('getCliques', async () => {
     const cliques = await api.getCliques()
     someBlockIds = cliques[0].block_ids
-    cliques.forEach((c) => Clique.check(c))
+    cliques.forEach((c) => Clique.strictCheck(c))
   })
 
   test('getGraphInterval', async () => {
     const interval = await api.getGraphInterval()
-    GraphInterval.check(interval)
+    GraphInterval.strictCheck(interval)
   })
 
   test('getStakers', async () => {
     const stakers = await api.getStakers({ limit: 5, offset: 0 })
-    stakers.forEach((s) => Staker.check(s))
+    stakers.forEach((s) => Staker.strictCheck(s))
     expect(stakers).toHaveLength(5)
   })
 
   test('getSlotTransfers', async () => {
     const transfers = await api.getSlotTransfers(lastSlot)
-    Transfers.check(transfers)
+    Transfers.strictCheck(transfers)
   })
 
   test('getMultipleSlotTransfers', async () => {
@@ -63,29 +63,29 @@ describe('unit tests', () => {
       lastSlot,
       { period: lastSlot.period - 1, thread: lastSlot.thread },
     ])
-    transfersArray.forEach((t) => Transfers.check(t))
+    transfersArray.forEach((t) => Transfers.strictCheck(t))
   })
 
   test('getBlockcliqueBlock', async () => {
     const block = await api.getBlockcliqueBlock(lastSlot)
     someEndorsement = block.header.content['endorsements']!
-    Block.check(block)
+    Block.strictCheck(block)
   })
 
   test('getBlock', async () => {
     const block = await api.getBlock(someBlockIds[0])
-    BlockInfo.check(block)
+    BlockInfo.strictCheck(block)
     expect(block).toHaveProperty('content.block.header.content.endorsements')
   })
 
   test('getMultipleBlocks', async () => {
     const blocks = await api.getMultipleBlocks(someBlockIds.slice(0, 2))
-    BlockInfo.check(blocks)
+    BlockInfo.strictCheck(blocks)
   })
 
   test('getEndorsement', async () => {
     const endorsement = await api.getEndorsement(someEndorsement[0].id!)
-    EndorsementChecker.check(endorsement)
+    EndorsementChecker.strictCheck(endorsement)
   })
 
   test('getMultipleEndorsements', async () => {
@@ -93,7 +93,7 @@ describe('unit tests', () => {
       someEndorsement[1].id!,
       someEndorsement[2].id!,
     ])
-    endorsements.forEach((e) => EndorsementChecker.check(e))
+    endorsements.forEach((e) => EndorsementChecker.strictCheck(e))
   })
 
   /*
