7b5adc01cf79f89d093baac1e5dcab970d5a2228
Grégory Libert <gl@massa.net>
2024-04-18 20:47:00 +0200

    Add client

---
+++ packages/massa-web3/src/experimental/client/client.ts
@@ -0,0 +1,22 @@
+import { OperationStatus } from '../basicElements'
+
+export interface SendOperationInput {
+  data: Uint8Array
+  publicKey: string
+  signature: string
+}
+
+export interface SCOutputEvent {
+  data: string
+}
+
+/*
+ * Blockchain client functions needed by the Operation class to send operations to the blockchain.
+ */
+export interface BlockchainClient {
+  sendOperation(data: SendOperationInput): Promise<string>
+  fetchChainId(): Promise<number>
+  fetchPeriod(): Promise<number>
+  getOperationStatus(operationId: string): Promise<OperationStatus>
+  getBalance(address: string, speculative?: boolean): Promise<number>
+}
