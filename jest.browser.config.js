e323e6c9ad57d0fb436bf4154e4fa924425a6f13
Pierre Seznec <petterseznec@gmail.com>
2024-04-19 15:36:53 +0200

    [exp] add coverage repport in PR

--- jest.browser.config.js
+++ jest.browser.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'jest-puppeteer',
-  testMatch: ['**/test/experimental/*.spec.ts'],
+  testMatch: ['**/test/experimental/unit/*.spec.ts'],
   transform: {
     '^.+\\.ts$': [
       'ts-jest',
