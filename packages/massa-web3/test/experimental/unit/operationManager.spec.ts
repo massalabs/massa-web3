8c15084e3732207b622000dd58c8acec866dee44
Grégory Libert <gl@massa.net>
2024-04-23 15:06:48 +0200

    560 xp add sc manager (#563)
    * Add smart contract
    
    * Fix review comments
    
    * put back getEvents
    
    * fix operation manager test
    
    * fix bigint
    
    * Move tests in right folder
    
    * [xp] rename waitFinalExecution
    
    * fix test by generating random secret key
    
    ---------
    
    Co-authored-by: BenRey <br@massa.net>
    Co-authored-by: Pierre Seznec <petterseznec@gmail.com>

---
+++ packages/massa-web3/test/experimental/unit/operationManager.spec.ts
@@ -0,0 +1,171 @@
+import {
+  IClientConfig,
+  IContractData,
+  IProvider,
+  ITransactionData,
+  OperationTypeId,
+  ProviderType,
+} from '../../../src/index'
+import { getOperationBufferToSign } from '../../../src/web3/accounts/Web3Account'
+import { BaseClient, PERIOD_OFFSET } from '../../../src/web3/BaseClient'
+
+import {
+  ExecuteOperation,
+  OperationManager,
+  OperationType,
+  TransferOperation,
+} from '../../../src/experimental/basicElements'
+import {
+  PrivateKey,
+  Address as XPAddress,
+} from '../../../src/experimental/basicElements'
+import 'dotenv/config'
+
+const clientConfig: IClientConfig = {
+  providers: [
+    { url: '', type: ProviderType.PUBLIC } as IProvider,
+    {
+      url: '',
+      type: ProviderType.PRIVATE,
+    } as IProvider,
+  ],
+  periodOffset: PERIOD_OFFSET,
+}
+
+describe('Unit tests', () => {
+  test('serialize - transfer', async () => {
+    const transfer: TransferOperation = {
+      fee: 1n,
+      type: OperationType.Transaction,
+      expirePeriod: 2,
+      amount: 3n,
+      recipientAddress: XPAddress.fromString(
+        'AU1wN8rn4SkwYSTDF3dHFY4U28KtsqKL1NnEjDZhHnHEy6cEQm53'
+      ),
+    }
+
+    const transactionData: ITransactionData = {
+      fee: 1n,
+      amount: 3n,
+      recipientAddress: 'AU1wN8rn4SkwYSTDF3dHFY4U28KtsqKL1NnEjDZhHnHEy6cEQm53',
+    }
+
+    expect(OperationManager.serialize(transfer)).toEqual(
+      Uint8Array.from(
+        new BaseClient(clientConfig).compactBytesForOperation(
+          transactionData,
+          OperationTypeId.Transaction,
+          2
+        )
+      )
+    )
+  })
+
+  test('serialize - execute', async () => {
+    const execute: ExecuteOperation = {
+      fee: 1n,
+      type: OperationType.ExecuteSmartContractBytecode,
+      expirePeriod: 2,
+      maxGas: 3n,
+      coins: 4n,
+      contractDataBinary: new Uint8Array([1, 2, 3, 4]),
+      datastore: new Map<Uint8Array, Uint8Array>([
+        [new Uint8Array([1, 2, 3, 4]), new Uint8Array([1, 2, 3, 4])],
+      ]),
+    }
+
+    const contractData: IContractData = {
+      fee: 1n,
+      maxGas: BigInt(3),
+      maxCoins: BigInt(4),
+      contractDataBinary: new Uint8Array([1, 2, 3, 4]),
+      datastore: new Map<Uint8Array, Uint8Array>([
+        [new Uint8Array([1, 2, 3, 4]), new Uint8Array([1, 2, 3, 4])],
+      ]),
+    }
+
+    expect(OperationManager.serialize(execute)).toEqual(
+      Uint8Array.from(
+        new BaseClient(clientConfig).compactBytesForOperation(
+          contractData,
+          OperationTypeId.ExecuteSC,
+          2
+        )
+      )
+    )
+  })
+
+  test('canonicalize', async () => {
+    const transfer: TransferOperation = {
+      fee: 1n,
+      type: OperationType.Transaction,
+      expirePeriod: 2,
+      amount: 3n,
+      recipientAddress: XPAddress.fromString(
+        'AU1wN8rn4SkwYSTDF3dHFY4U28KtsqKL1NnEjDZhHnHEy6cEQm53'
+      ),
+    }
+    const publicKey = await PrivateKey.generate().getPublicKey()
+    expect(OperationManager.canonicalize(1n, transfer, publicKey)).toEqual(
+      Uint8Array.from(
+        getOperationBufferToSign(
+          1n,
+          publicKey.toBytes(),
+          Buffer.from(
+            new BaseClient(clientConfig).compactBytesForOperation(
+              {
+                fee: 1n,
+                amount: 3n,
+                recipientAddress:
+                  'AU1wN8rn4SkwYSTDF3dHFY4U28KtsqKL1NnEjDZhHnHEy6cEQm53',
+              },
+              OperationTypeId.Transaction,
+              2
+            )
+          )
+        )
+      )
+    )
+  })
+
+  test('canonicalize - execute', async () => {
+    const execute: ExecuteOperation = {
+      fee: 1n,
+      type: OperationType.ExecuteSmartContractBytecode,
+      expirePeriod: 2,
+      maxGas: 3n,
+      coins: 4n,
+      contractDataBinary: new Uint8Array([1, 2, 3, 4]),
+      datastore: new Map<Uint8Array, Uint8Array>([
+        [new Uint8Array([1, 2, 3, 4]), new Uint8Array([1, 2, 3, 4])],
+      ]),
+    }
+
+    const contractData: IContractData = {
+      fee: 1n,
+      maxGas: BigInt(3),
+      maxCoins: BigInt(4),
+      contractDataBinary: new Uint8Array([1, 2, 3, 4]),
+      datastore: new Map<Uint8Array, Uint8Array>([
+        [new Uint8Array([1, 2, 3, 4]), new Uint8Array([1, 2, 3, 4])],
+      ]),
+    }
+
+    const publicKey = await PrivateKey.generate().getPublicKey()
+    expect(OperationManager.canonicalize(1n, execute, publicKey)).toEqual(
+      Uint8Array.from(
+        getOperationBufferToSign(
+          1n,
+          publicKey.toBytes(),
+          Buffer.from(
+            new BaseClient(clientConfig).compactBytesForOperation(
+              contractData,
+              OperationTypeId.ExecuteSC,
+              2
+            )
+          )
+        )
+      )
+    )
+  })
+})
