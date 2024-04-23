6846b1e90a6d66d2437d3777457afb6ff4305547
BenRey <br@massa.net>
2024-04-23 16:59:08 +0200

    complete address test

--- packages/massa-web3/test/experimental/unit/address.spec.ts
+++ packages/massa-web3/test/experimental/unit/address.spec.ts
@@ -1,7 +1,16 @@
 import { Account } from '../../../src/experimental/account'
 import { Address } from '../../../src/experimental/basicElements'
+import { Version } from '../../../src/experimental/crypto/interfaces/versioner'
+
 import { Address as LegacyAddress } from '../../../src/utils/keyAndAddresses'
 
+const unsupported_version = -1 as Version
+
+class TestAddress extends Address {
+  public static callInitFromVersion(version: Version = Version.V0): Address {
+    return this.initFromVersion(version)
+  }
+}
 describe('Address tests', () => {
   let account: Account
 
@@ -9,7 +18,7 @@ describe('Address tests', () => {
     account = await Account.generate()
   })
 
-  test('Serialization', async () => {
+  test('serialization and deserialization', async () => {
     const addStr = account.address.toString()
     const address = Address.fromString(addStr)
     const legacyAddress = new LegacyAddress(addStr)
@@ -20,5 +29,54 @@ describe('Address tests', () => {
 
     // deserialization
     expect(Address.fromBytes(bytes)).toStrictEqual(address)
+  })
+
+  test('initFromVersion throws error for unsupported version', () => {
+    expect(() => TestAddress.callInitFromVersion(unsupported_version)).toThrow(
+      `Unsupported version: ${unsupported_version}`
+    )
+  })
+
+  test('fromString throws error for invalid address string', () => {
+    expect(() => Address.fromString('invalid_address_string')).toThrow(
+      'invalid address string:'
+    )
+  })
+
+  test('toString returns string with user prefix for EOA', () => {
+    const address = Address.fromPublicKey(account.publicKey)
+    address.isEOA = true
+    expect(address.toString()).toMatch(/^AU/)
+  })
+
+  test('toString returns string with contract prefix for contract account', () => {
+    const address = Address.fromPublicKey(account.publicKey)
+    address.isEOA = false
+    expect(address.toString()).toMatch(/^AS/)
+  })
+
+  test('toBytes returns bytes with EOA prefix for EOA', () => {
+    const address = Address.fromPublicKey(account.publicKey)
+    const bytes = address.toBytes()
+
+    // The first byte should be 0 for EOA
+    expect(bytes[0]).toBe(0)
+  })
+
+  test('toBytes returns bytes with contract prefix for contract account', () => {
+    const address = Address.fromPublicKey(account.publicKey)
+    address.isEOA = false
+
+    const bytes = address.toBytes()
+    // The first byte should be 1 for contract account
+    expect(bytes[0]).toBe(1)
+  })
+
+  test('getByteLength returns correct length', () => {
+    const address = Address.fromPublicKey(account.publicKey)
+    const bytes = address.toBytes()
+    const byteLength = Address.getByteLength(bytes)
+
+    expect(byteLength).toBe(bytes.length)
   })
 })
