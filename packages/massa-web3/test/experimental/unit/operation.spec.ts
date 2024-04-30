940bcceb925e24995b9f3a16a028cff75a3538d4
ASAPSegfault <45430752+ASAPSegfault@users.noreply.github.com>
2024-04-30 16:05:08 +0200

    add operation object unit tests

---
+++ packages/massa-web3/test/experimental/unit/operation.spec.ts
@@ -0,0 +1,37 @@
+import { Operation } from '../../../src/experimental/basicElements'
+import { blockchainClientMock } from './mock/blockchainClient.mock'
+
+const OPERATION_ID = 'testOperationID'
+
+describe('Unit tests', () => {
+  const operation = new Operation(blockchainClientMock, OPERATION_ID)
+
+  test('get operation status', async () => {
+    const status = await operation.getStatus()
+
+    expect(blockchainClientMock.getOperationStatus).toHaveBeenCalledWith(
+      OPERATION_ID
+    )
+    expect(status).toBeDefined()
+  })
+
+  test('get speculative events', async () => {
+    const events = await operation.getSpeculativeEvents()
+
+    expect(blockchainClientMock.getEvents).toHaveBeenCalledWith({
+      operationId: OPERATION_ID,
+      isFinal: false,
+    })
+    expect(events).toStrictEqual([])
+  })
+
+  test('get final events', async () => {
+    const events = await operation.getFinalEvents()
+
+    expect(blockchainClientMock.getEvents).toHaveBeenCalledWith({
+      operationId: OPERATION_ID,
+      isFinal: true,
+    })
+    expect(events).toStrictEqual([])
+  })
+})
