2ea7d373c9ece04e1c44fc898ec782a57c9ab5fa
Grégory Libert <gl@massa.net>
2024-05-03 15:24:44 +0200

    Fix CR

--- packages/massa-web3/src/experimental/basicElements/mas.ts
+++ packages/massa-web3/src/experimental/basicElements/mas.ts
@@ -1,45 +1,124 @@
+import { FIRST, ONE } from '../utils'
+
 const NB_DECIMALS = 9
+
+export const ERROR_NOT_SAFE_INTEGER = 'value is not a safe integer.'
+export const ERROR_VALUE_TOO_LARGE = 'value is too large.'
 
 export type Mas = bigint
 
+/**
+ * Converts an integer value to the smallest unit of the Massa currency.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isSafeInteger#description
+ *
+ * @param value - The integer value.
+ * @returns The value in the smallest unit of the Massa currency.
+ *
+ * @throws An error if the value is not a safe integer.
+ */
 export function fromMas(value: number): Mas {
   if (!Number.isSafeInteger(value)) {
-    throw new Error('value is not a safe integer.')
+    throw new Error(ERROR_NOT_SAFE_INTEGER)
   }
 
   return BigInt(value * 10 ** NB_DECIMALS)
 }
 
+/**
+ * Converts an integer value in milli Massa to the smallest unit of the Massa currency.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isSafeInteger#description
+ *
+ * @param value - The value in milli Massa.
+ * @returns The value in the smallest unit of the Massa currency.
+ *
+ * @throws An error if the value is not a safe integer.
+ */
 export function fromMilliMas(value: number): Mas {
   if (!Number.isSafeInteger(value)) {
-    throw new Error('value is not a safe integer.')
+    throw new Error(ERROR_NOT_SAFE_INTEGER)
   }
 
   return BigInt(value * 10 ** (NB_DECIMALS - 3))
 }
 
+/**
+ * Converts an integer value in micro Massa to the smallest unit of the Massa currency.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isSafeInteger#description
+ *
+ * @param value - The value in micro Massa.
+ * @returns The value in the smallest unit of the Massa currency.
+ *
+ * @throws An error if the value is not a safe integer.
+ */
 export function fromMicroMas(value: number): Mas {
   if (!Number.isSafeInteger(value)) {
-    throw new Error('value is not a safe integer.')
+    throw new Error(ERROR_NOT_SAFE_INTEGER)
   }
-
   return BigInt(value * 10 ** (NB_DECIMALS - 6))
 }
 
+/**
+ * Converts an integer value in nano Massa to the smallest unit of the Massa currency.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isSafeInteger#description
+ *
+ * @param value - The value in nano Massa.
+ * @returns The value in the smallest unit of the Massa currency.
+ *
+ * @throws An error if the value is not a safe integer.
+ */
 export function fromNanoMas(value: number): Mas {
   if (!Number.isSafeInteger(value)) {
-    throw new Error('value is not a safe integer.')
+    throw new Error(ERROR_NOT_SAFE_INTEGER)
   }
 
   return BigInt(value * 10 ** (NB_DECIMALS - 9))
 }
 
+/**
+ * Converts a decimal value to the smallest unit of the Massa currency.
+ *
+ * @param value - The decimal value.
+ * @returns The value in the smallest unit of the Massa currency.
+ *
+ * @throws An error if the format is not a decimal.
+ * @throws An error if the value is too large to be represented by an U256 or has too many decimals.
+ */
 export function fromString(value: string): Mas {
-  const [integerPart, decimalPart = ''] = value.split('.')
-  return BigInt(integerPart + decimalPart.padEnd(NB_DECIMALS, '0'))
+  const parts = value.split('.')
+  if (parts.length > 2) {
+    throw new Error('invalid format')
+  }
+
+  const integerPart = parts[FIRST]
+  const decimalPart = parts[ONE] || ''
+  if (decimalPart.length > NB_DECIMALS) {
+    throw new Error(ERROR_VALUE_TOO_LARGE)
+  }
+
+  const mas = BigInt(integerPart + decimalPart.padEnd(NB_DECIMALS, '0'))
+  if (mas >= BigInt(1) << BigInt(256)) {
+    throw new Error(ERROR_VALUE_TOO_LARGE)
+  }
+
+  return mas
 }
 
+/**
+ * Converts a value in the smallest unit of the Massa currency to a decimal value.
+ *
+ * @param value - The value in the smallest unit of the Massa currency.
+ * @returns The decimal value.
+ *
+ * @throws An error if the value is too large to be represented by an U256.
+ */
 export function toString(value: Mas): string {
+  if (value >= BigInt(1) << BigInt(256)) {
+    throw new Error(ERROR_VALUE_TOO_LARGE)
+  }
   const valueString = value.toString()
   const integerPart = valueString.slice(0, -NB_DECIMALS) || '0'
   const decimalPart = valueString.slice(-NB_DECIMALS).replace(/0+$/, '')
--- packages/massa-web3/test/experimental/unit/mas.spec.ts
+++ packages/massa-web3/test/experimental/unit/mas.spec.ts
@@ -1,22 +1,22 @@
-import * as Mas from '../../../src/experimental/basicElements/mas'
+import { Mas } from '../../../src/experimental/basicElements'
 
 describe('amount conversion', () => {
   it('converts from integer', () => {
     expect(Mas.fromMas(1)).toStrictEqual(1_000_000_000n)
     expect(Mas.fromMas(1234)).toStrictEqual(1_234_000_000_000n)
-    expect(() => Mas.fromMas(1.1)).toThrow('value is not a safe integer.')
+    expect(() => Mas.fromMas(1.1)).toThrow(Mas.ERROR_NOT_SAFE_INTEGER)
 
     expect(Mas.fromMilliMas(1)).toStrictEqual(1_000_000n)
     expect(Mas.fromMilliMas(1234)).toStrictEqual(1_234_000_000n)
-    expect(() => Mas.fromMilliMas(1.1)).toThrow('value is not a safe integer.')
+    expect(() => Mas.fromMilliMas(1.1)).toThrow(Mas.ERROR_NOT_SAFE_INTEGER)
 
     expect(Mas.fromMicroMas(1)).toStrictEqual(1_000n)
     expect(Mas.fromMicroMas(1234)).toStrictEqual(1_234_000n)
-    expect(() => Mas.fromMicroMas(1.1)).toThrow('value is not a safe integer.')
+    expect(() => Mas.fromMicroMas(1.1)).toThrow(Mas.ERROR_NOT_SAFE_INTEGER)
 
     expect(Mas.fromNanoMas(1)).toStrictEqual(1n)
     expect(Mas.fromNanoMas(1234)).toStrictEqual(1_234n)
-    expect(() => Mas.fromNanoMas(1.1)).toThrow('value is not a safe integer.')
+    expect(() => Mas.fromNanoMas(1.1)).toThrow(Mas.ERROR_NOT_SAFE_INTEGER)
   })
 
   it('converts from string', () => {
@@ -24,11 +24,14 @@ describe('amount conversion', () => {
     expect(Mas.fromString('1.1')).toStrictEqual(1_100_000_000n)
     expect(Mas.fromString('01234.56789')).toStrictEqual(1_234_567_890_000n)
     expect(Mas.fromString('01234.567890000')).toStrictEqual(1_234_567_890_000n)
+    expect(() => Mas.fromString('1.1.1')).toThrow()
+    expect(() => Mas.fromString('0.1234567890')).toThrow()
   })
 
   it('converts to string', () => {
     expect(Mas.toString(1_000_000_000n)).toStrictEqual('1')
     expect(Mas.toString(1_100_000_000n)).toStrictEqual('1.1')
     expect(Mas.toString(1_234_567_890_000n)).toStrictEqual('1234.56789')
+    expect(() => Mas.toString(BigInt(1) << BigInt(256))).toThrow()
   })
 })
