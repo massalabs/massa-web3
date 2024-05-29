a78d891065db6936bb2b8227ee3402469fd6fd29
BenRey <br@massa.net>
2024-05-29 14:15:38 +0200

    fix path

--- packages/massa-web3/src/deployerGeneration/generate-deployer.ts
+++ packages/massa-web3/src/deployerGeneration/generate-deployer.ts
@@ -1,6 +1,7 @@
 import path from 'path'
 import fs from 'fs'
 
+console.log('Generating deployer bytecode...')
 function populateDeployer(src?: string, dst?: string): void {
   const wasmPath = src ?? path.join(__dirname, 'deployer.wasm')
   const wasmData = fs.readFileSync(wasmPath)
@@ -8,7 +9,7 @@ function populateDeployer(src?: string, dst?: string): void {
   const output = `export const deployer: Uint8Array = new Uint8Array([${[...wasmData]}]);\n`
 
   const outputDir =
-    dst ?? path.join(__dirname, '../src/generated/deployer-bytecode.ts')
+    dst ?? path.join(__dirname, '../generated/deployer-bytecode.ts')
 
   fs.writeFileSync(outputDir, output)
 }
