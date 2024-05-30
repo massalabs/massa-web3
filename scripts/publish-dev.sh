88ec01e59c6ce0b0268ac0bc070c0d78db00db88
BenRey <br@massa.net>
2024-05-30 14:51:05 +0200

    clean publish script

--- scripts/publish-dev.sh
+++ scripts/publish-dev.sh
@@ -12,14 +12,5 @@ npm version --preid dev --no-git-tag-version --no-commit-hooks prepatch
 TIME=$(date -u +%Y%m%d%H%M%S)
 sed -i "/version/s/dev.0/dev.$TIME/g" package.json
 
-# Determine the branch and set the appropriate tag
-BRANCH=${GITHUB_REF##*/}
-TAG=""
-if [[ "$BRANCH" == "buildnet" ]]; then
-  TAG="buildnet-"
-elif [[ "$BRANCH" == "testnet" ]]; then
-  TAG="testnet-"
-fi
-
 # Publish the package
 npm publish --access public --tag ${TAG}dev
