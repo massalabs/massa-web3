3f24485873d78e8d7a500436b750996e2cf4e8eb
thomas-senechal <thomas.senechal@pm.me>
2024-05-31 10:18:21 +0200

    Remove old open_rpc tests from pkg/massa-web3

--- packages/massa-web3/jest.config.ts
+++ packages/massa-web3/jest.config.ts
@@ -6,9 +6,6 @@ const config: Config.InitialOptions = {
   rootDir: '../..',
   displayName: 'massa-web3',
   testMatch: ['<rootDir>/packages/massa-web3/test/**/*.(spec|test).ts?(x)'],
-  testPathIgnorePatterns: [
-    '<rootDir>/packages/massa-web3/test/open_rpc/publicAPI.spec.ts',
-  ],
 }
 
 export default config
