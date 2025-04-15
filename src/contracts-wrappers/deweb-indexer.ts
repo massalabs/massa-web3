9d4fc804f2fbf52e158aabb9d1803faef4f7d120
Peterjah <ps@massa.net>
2025-04-15 12:33:05 +0200

    add deweb indexer contracts

---
+++ src/contracts-wrappers/deweb-indexer.ts
@@ -0,0 +1,8 @@
+import { NetworkName } from '../utils'
+
+export const DEWEB_INDEXER_CONTRACTS = {
+  [NetworkName.Mainnet]:
+    'AS12UpEfdonZxyFnsmrJfZbXLM3Gq6LaL3hPk7wTXqU4UZfnypKzF',
+  [NetworkName.Buildnet]:
+    'AS1TmA4GNpSYBseNNMXpbAp2trUwZxZy3T1sZ9Qd3Qdn9L8wGbMS',
+}
--- src/contracts-wrappers/index.ts
+++ src/contracts-wrappers/index.ts
@@ -1,3 +1,4 @@
 export * from './token'
 export * from './tokens'
 export * from './mns'
+export * from './deweb-indexer'
